a45375df59a30279fdc0877b282be6366a658952
Bob McWhirter <bob@fnokd.com>
2010-03-06 13:03:35 -0500

    Cleaning up post date display. Styling each post a tad more. Changing per-page from 5 to 10.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,7 +2,7 @@ require 'odd_archive_posts'
 
 Awestruct::Extensions::Pipeline.new do
   extension OddArchivePosts.new( '/posts' ) 
-  extension Awestruct::Extensions::Paginator.new( :posts, '/posts/index', :per_page=>5 )
+  extension Awestruct::Extensions::Paginator.new( :posts, '/posts/index', :per_page=>10 )
   extension Awestruct::Extensions::Indexifier.new
 end
 
